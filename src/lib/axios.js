34c72073c6eb9626e61a446146bddab3e15d659c
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-08-26 00:51:07 +0530

    refactor : axios url updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "https://pixster-main-be.onrender.com",
+    baseURL : import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : "https://pixster-main-be.onrender.com/api",
     withCredentials : true
 });
